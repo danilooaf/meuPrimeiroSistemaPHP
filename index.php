321c8e2837fd8852e35131086260d1f491044c65
Danilo <danilooaf@hotmail.com>
2021-02-09 12:53:48 -0300

    Commit OK

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gama Academy</title>
+
+    <!--Como fazer uma chamada de biblioteca em CSS-->
+    <link rel="Stylesheet" href="../css/estilos.css">
+
+</head>
+<body>
+
+    <!--criando o menu principal da página-->
+    <table border="0" width="100%">
+        <tr>
+            <td><img src="../imagens/logo.png" width="200" height="40"></td>
+            <td>QUEM SOMOS</td>
+            <td>CURSOS ONLINE</td>
+            <td>PARA EMPRESAS</td>
+            <td><!-- Inserindo um link para a página de contato-->
+                <a href="../html/contato.html">CONTATO</td></a>
+            <td>CONTEÚDOS</td>
+                    </tr>
+    </table>
+
+     <!-- Criando uma tabela na nossa página-->
+     <table border="1" width="100%">
+        <!-- <tr> permite criar linhas-->
+        <tr>
+            <!-- <td> permite criar colunas-->
+                <td align="center"><!--Inserindo uma imagem na página-->
+                    <img src="../imagens/pessoas.png"> </td>
+                <td><!--Inserir um título-->
+                    <h1>ISTO NÃO É<br> UMA ESCOLA</h1><!--Tamanhos de títulos
+                    <h2>ISTO NÃO É UMA ESCOLA</h2>
+                    <h3>ISTO NÃO É UMA ESCOLA</h3>
+                    <h4>ISTO NÃO É UMA ESCOLA</h4>
+                    <h5>ISTO NÃO É UMA ESCOLA</h5>
+                    <h6>ISTO NÃO É UMA ESCOLA</h6>-->
+                    
+                    <!-- Inserindo parágrafos-->
+                    <p>Esse lema nos trouxe até aqui!<br><!-- <br> quebra de linha-->
+                        Somos diferentes e visionários </p>
+                
+                    <p>Tão inexplicáveis que <b>era mais simples dizer o que não éramos</b></p>
+                
+                    <p>Marca inovadora e questionadora por essência, questionou a si mesmo<br>
+                        sobre <b>como se reinventar</b> em um momento de <strike>crise</strike> oportunidade para<br>
+                        chegar ainda mais perto do seu objetivo de impactar 1 milhão de vidas.</p>
+
+                    <p>Não éramos uma escola, éramos uma experiência diferenciada de aprendizagem:<br>
+                    <i>“menos conversinha e mais conversão!”</i></p>
+
+                    
+                    <!-- inserindo um componente de vídeo-->
+                    <video controls= "autoplay="true" width="400" height="300">
+                        <source src="../videos/Gama.mp4"/>
+
+                    </video>
+                        <!--inserindo botão napágina com link-->   
+                        <a href="https://www.youtube.com/watch?v=YmfBy9mihL0">
+                        <img src="../imagens/play.jpg" width="40" height="40">
+                        </a>
+                    </td>
+        </tr>
+    </table>
+
+    
+
+
+    
+
+    
+</body>
+</html>
